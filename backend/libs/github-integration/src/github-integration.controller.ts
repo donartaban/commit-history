ae48b969b2ce6debfde5fa0490583bbf71325595
Artaban <ggantus89@gmail.com>
2022-11-24 23:09:51 -0600

    implement repos and commits lists from a personal access tokens

--- backend/libs/github-integration/src/github-integration.controller.ts
+++ backend/libs/github-integration/src/github-integration.controller.ts
@@ -1,19 +1,30 @@
 import { HttpExceptionFilter } from "@app/common";
-import { Controller, Get, Query, UseFilters } from "@nestjs/common";
+import { Controller, Get, Param, Query, UseFilters } from "@nestjs/common";
 import { GithubIntegrationService } from "./github-integration.service";
 
 @Controller('github')
 @UseFilters(HttpExceptionFilter)
-export class GithubIntegrationController{
+export class GithubIntegrationController {
 
     constructor(
         private readonly githubService: GithubIntegrationService
-    ){}
+    ) { }
 
     @Get('repositories')
     async listRepositories(
-        @Query('page') page: string
-    ){
-        return this.githubService.listRepositories(parseInt(page));
+        @Query('page') page: string,
+        @Query('per_page') per_page: string
+    ) {
+        return this.githubService.listRepositories(parseInt(page), parseInt(per_page));
+    }
+
+    @Get('repository/:id/owner/:owner/commits')
+    async listCommitsFromRepository(
+        @Param('id') repo: string,
+        @Param('owner') owner: string,
+        @Query('page') page: string,
+        @Query('per_page') per_page: string
+    ) {
+        return this.githubService.listCommits(repo, owner, parseInt(page), parseInt(per_page));
     }
 }
--- backend/libs/github-integration/src/github-integration.service.ts
+++ backend/libs/github-integration/src/github-integration.service.ts
@@ -9,12 +9,31 @@ export class GithubIntegrationService {
         auth: process.env.GITHUB_TOKEN
     });
 
-    constructor() { }
-
-    async listRepositories(page: number) {
-        const [error, repos] = await to(this.Octokit.request('GET /user/repos', { type: 'public' }));
+    /**
+     * List all public repositories from github account accessing throgth personal access token
+     * @param page {number}
+     * @param per_page {number}
+     * @returns {Promise<any>}
+     */
+    async listRepositories(page: number = 1, per_page: number = 20): Promise<any> {
+        const [error, repos] = await to(this.Octokit.request('GET /user/repos', { type: 'public', page, per_page, sort: 'created', direction: 'desc' }));
         if (error)
             throw new HttpException('Something went wrong trying to get public repositories, please try again', HttpStatus.BAD_REQUEST);
-        return repos;
+        return repos && repos.data ? repos.data : [];
+    }
+
+    /**
+     * List all commits from specific owner and repo
+     * @param repo {string}
+     * @param owner {number}
+     * @param page {number}
+     * @param per_page {number}
+     * @returns {Promise<any>}
+     */
+    async listCommits(repo: string, owner: string, page: number = 1, per_page: number = 20): Promise<any> {
+        const [error, commits] = await to(this.Octokit.request('GET /repos/{owner}/{repo}/commits', { owner, repo, page, per_page }));        
+        if (error)
+            throw new HttpException('Something went wrong trying to get commits from specific repo, please try again', HttpStatus.BAD_REQUEST);
+        return commits && commits.data ? commits.data : [];
     }
 }
